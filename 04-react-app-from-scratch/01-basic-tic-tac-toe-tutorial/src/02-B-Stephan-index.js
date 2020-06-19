19d25ab3ff933ccfae04c538a98fd5bebd15b26f
Stephan Pannier <stephan.pannier@ecx.io>
2020-06-19 12:09:48 +0200

    Added tutorial on https://reactjs.org/tutorial/tutorial.html in different steps (see therefore the number before file '<number>-index.js'

---
+++ 04-react-app-from-scratch/01-basic-tic-tac-toe-tutorial/src/02-B-Stephan-index.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class Square extends React.Component {
+    render() {
+        return (
+            <button className="square" style={{backgroundColor: "green"}}>
+                {this.props.value}
+            </button>
+        );
+    }
+}
+
+class Stephan extends React.Component{
+    render(){
+        return(
+            <div className="stephanTest">
+                <h2>Eigene Erweiterung von Stephan</h2>
+                Das ist ein Test von Stephan
+            </div>
+        );
+    }
+}
+
+class Board extends React.Component {
+    renderSquare(i) {
+        return <Square value={i}/>;
+    }
+
+    render() {
+        const status = 'Next player: X';
+
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                    {this.renderSquare(3)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(8)}
+                    {this.renderSquare(9)}
+                    {this.renderSquare(10)}
+                    {this.renderSquare(11)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(12)}
+                    {this.renderSquare(13)}
+                    {this.renderSquare(14)}
+                    {this.renderSquare(15)}
+                </div>
+            </div>
+        );
+    }
+}
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board/>
+                </div>
+                <div className="game-info">
+                    <div>{/* status */}</div>
+                    <ol>{/* TODO */}</ol>
+                </div>
+
+                <div className="teststephan">
+                    <Stephan/>
+                </div>
+            </div>
+        );
+    }
+}
+
+// ========================================
+
+ReactDOM.render(
+    <Game/>,
+    document.getElementById('root')
+);
